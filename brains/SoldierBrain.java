88dacfb24e78a3ecbd05b2169414517c78626be9
Drew Harry <drew.harry@gmail.com>
2012-01-16 12:37:11 -0500

    Made new bits for soldier robots.

---
+++ brains/SoldierBrain.java
@@ -0,0 +1,16 @@
+package team035.brains;
+
+import team035.robots.BaseRobot;
+
+public class SoldierBrain extends RobotBrain {
+
+	public SoldierBrain(BaseRobot r) {
+		super(r);
+	}
+	
+	@Override
+	public void think() {
+
+	}
+
+}
