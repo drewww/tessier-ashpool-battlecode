48d1e72d058f5d2a2711180823d012f5dff2b5f0
Owen Macindoe <omacindoe@gmail.com>
2012-01-23 19:22:05 -0500

    Added a whole bunch of robots

---
+++ robots/ScoutRobot.java
@@ -0,0 +1,24 @@
+package team035.robots;
+
+import team035.brains.ScoutBrain;
+import team035.modules.NavController;
+import team035.modules.RadarController;
+import team035.modules.RadioController;
+import team035.modules.StateCache;
+import battlecode.common.RobotController;
+
+public class ScoutRobot extends BaseRobot {
+
+	public ScoutRobot(RobotController myRc) {
+		super(myRc);
+		
+		this.radar = new RadarController(this);
+		this.radio = new RadioController(this);
+		this.cache = new StateCache(this);
+		this.nav = new NavController(this);
+		
+		this.brain = new ScoutBrain(this);
+
+	}
+
+}
